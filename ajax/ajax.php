275cf7cc55c819c1fd5b09a3bbc53ea819c589d4
anwaar12345 <syedanwar016@gmail.com>
2020-03-17 16:56:28 +0500

    Jquery And AJAX BAsics

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -1,21 +1,46 @@
 <?php
 
-//  $name = addcslashes($_POST['name']," ");
-//  $name = strip_tags($_POST['name']);
+
 // $name = $_POST['name'];
-$name = htmlentities($_POST['name']);
+// $name = htmlentities($_POST['name']);
 // $name =  htmlspecialchars($_POST['name']);
+// $name = $_POST['name'];
+// // $name =  addcslashes($_POST['name']," ");
+// // $name = stripslashes($_POST['name']);
+// if(empty($name)){
+//     die("required");
+// }else{
 
-// $name =  addcslashes($_POST['name']," ");
-// $name = stripslashes($_POST['name']);
-if(empty($name)){
-    die("required");
-}else{
+//     echo "from Ajax Data --- $name"
+// }
 
-    echo "from Ajax Data --- $name";
+$name = $_POST['name'];
+ function testinput($name)
+{
 
+if(!empty($name)){
+
+$name = htmlentities($name);
+
+$name = addcslashes($name," ");
+
+$name = trim($name);
+
+//$name =  mysqli_real_escape_string($name);
+// $name = addcslashes($name," ");
+
+// $name = strip_tags($name);
+
+// $name = htmlspecialchars($name);
+
+// $name = stripslashes($name);
+
+
+return "data from AJAX -> ".$name;
+
+}elseif(empty($name)){
+    echo "Fill Out The Field";
 }
-
-
-
+}
+echo testinput($name);
 ?>
