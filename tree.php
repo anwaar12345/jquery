f9415fa9d38b1d444038cf82e3975875244e4c05
anwarshah <syed.anwar@nextgeni.dev>
2020-06-05 18:11:55 +0500

    improvements in code

--- tree.php
+++ tree.php
@@ -1,4 +1,5 @@
 <?php
+if(isset($_POST['submit'])){
 
     class Node {
 
@@ -26,7 +27,9 @@
           public $root;
 
           public function  __construct() {
-                 $this->root = NULL;
+                 $this->root =  new Node(null);
+                 
+
           }
   
           public function create($info) {
@@ -69,9 +72,8 @@
 
           public function traverse() {
 
-                     $this->inorder($this->root);
-                 
-
+            $this->inorder($this->root);
+               
           } 
 
           private function inorder($node) {
@@ -86,14 +88,34 @@
                              $this->inorder($node->right); 
                           } 
           }
+          public function hasNode() {
+            return (bool)$this->root->right;
 
-
+         }
+          public function printTree() {
+            if ($this->hasNode()) {
+               $current_level[] = $this->root->right;
+               
+               $next_level = array();
+               while (!empty($current_level)) {
+                  $node = array_shift($current_level);
+                  if ($node) {
+                     array_push($next_level, $node->left, $node->right);
+                     echo $node . ' ';
+                  }
+                  if (empty($current_level) && !empty($next_level)) {
+                     echo "\n";
+                     list($current_level, $next_level) = array($next_level, $current_level);
+                  }
+               }
+            }
+         }
 
 
           
 
     } 
-               $arr = array(8,3,1,6,4,7,10,14,13);
+               $arr = explode(",",$_POST['arr']);
 
                $tree = new SearchBinaryTree();
                for($i=0,$n=count($arr);$i<$n;$i++) {
@@ -101,14 +123,35 @@
                }
 
 
-    echo "<h2>Input: ", join($arr," "), "</h2>";
+    echo "<h2>Input: ", implode(",",$arr), "</h2>";
 
     
 
     echo"<h1>Inorder</h1>"; 
     $tree->traverse();
 
-
- 
+echo "<h1>Binary Tree</h1>";
+$tree->printTree(); 
+}
   
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Tree Sort</title>
+</head>
+<body>
+<h3>In Tree Sort We always Follow Binary Search Tree and for Travesing we always Do Inorder Traversing</h3>
+<p><b>What is In-Order Traversing ?</p><br>In order traversing is a Technique For Traversing Tree in this we do visit our left node and the node and then the Right Node.<br/>in this we always check if There is a left node if no we print root node.</p>
+<form action="" method="post">
+<input type="text" name="arr" placeholder="Enter Numbers to make tree and traverse ...!" required>  
+<input type="submit" name="submit">
+</form> 
+</body>
+</html>
